118a37f48ff37d78960e62e29e2f69124b4b86b9
Julian Jendryka <65280810+JulianJendryka@users.noreply.github.com>
2024-01-05 15:11:30 +0100

    Day_09

---
+++ 09/rust/src/part_2.rs
@@ -0,0 +1,46 @@
+use std::fs;
+
+fn read_text(file_path: String) -> String {
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    return contents;
+}
+
+fn parse_input(input: String) -> Vec<Vec<i128>> {
+    input
+        .split("\r\n")
+        .into_iter()
+        .map(|line| {
+            line.split(" ")
+                .map(|num| num.parse::<i128>().unwrap())
+                .into_iter()
+                .collect()
+        })
+        .collect()
+}
+
+fn find_sequence_ending(sequence: &Vec<i128>) -> i128 {
+    if sequence.iter().all(|num| *num == 0) {
+        return 0;
+    }
+
+    let differences: Vec<i128> = sequence
+        .iter()
+        .zip(sequence.iter().skip(1))
+        .map(|(current, next)| next - current)
+        .collect();
+
+    return differences.first().unwrap() - find_sequence_ending(&differences);
+}
+
+pub fn main() {
+    println!("PART 2 ------------");
+    let input = read_text("..\\Data\\input_1.txt".to_string());
+
+    let readings = parse_input(input);
+
+    let sum: i128 = readings
+        .into_iter()
+        .map(|sequence: Vec<i128>| sequence.first().unwrap() - find_sequence_ending(&sequence))
+        .sum();
+    println!("sum {:?}", sum);
+}
